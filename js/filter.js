a4dc21b8b57dcb999cb8e1c7b68de0491bb6435a
Yurbazzzz <Yurbazzzz@users.noreply.github.com>
2022-02-01 22:30:47 +0200

    .

--- js/filter.js
+++ js/filter.js
@@ -7,8 +7,77 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
 
+function showAll() {
 
+}
 
 function grass() {
-  document.getElementsByClassName('.grass').style.color = 'red';
+  let grass = document.getElementById('grass');
+  grass.classList.add('show');
+}
+
+
+function fire() {
+  let fire = document.getElementById('fire');
+  fire.classList.add('show');
+}
+
+function water() {
+  let water = document.getElementById('water');
+  water.classList.add('show');
+}
+
+function electric() {
+  let electric = document.getElementById('electric');
+  electric.classList.add('show');
+}
+
+function ground() {
+  let ground = document.getElementById('ground');
+  ground.classList.add('show');
+}
+
+function rock() {
+  let rock = document.getElementById('rock');
+  rock.classList.add('show');
+}
+
+function fairy() {
+  let fairy = document.getElementById('fairy');
+  fairy.classList.add('show');
+}
+
+function poison() {
+  let poison = document.getElementById('poison');
+  poison.classList.add('show');
+}
+
+function bug() {
+  let bug = document.getElementById('bug');
+  bug.classList.add('show');
+}
+
+function dragon() {
+  let dragon = document.getElementById('dragon');
+  dragon.classList.add('show');
+}
+
+function psychic() {
+  let psychic = document.getElementById('psychic');
+  psychic.classList.add('show');
+}
+
+function flying() {
+  let flying = document.getElementById('flying');
+  flying.classList.add('show');
+}
+
+function fighting() {
+  let fighting = document.getElementById('fighting');
+  fighting.classList.add('show');
+}
+
+function normal() {
+  let normal = document.getElementById('normal');
+  normal.classList.add('show');
 }
